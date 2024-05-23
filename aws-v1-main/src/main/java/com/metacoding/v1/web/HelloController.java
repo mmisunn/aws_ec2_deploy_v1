41f533fe686139d2b1f3b317543699626b781475
choi misun <missun0707@gamil.com>
2024-05-23 10:11:53 +0900

    aa

--- aws-v1-main/src/main/java/com/metacoding/v1/web/HelloController.java
+++ aws-v1-main/src/main/java/com/metacoding/v1/web/HelloController.java
@@ -15,12 +15,14 @@ public class HelloController {
     public String hello(@RequestParam(defaultValue = "1") Integer number){
         if(number == 1){ // info 로그
             log.info("/aws/v1 이 호출되었어요. info 로그 #####################################");
+			//return "<h1>number 값이 1로 요청했습니다.</h1>";
         }else if(number == -1){ // error 로그
             log.error("/aws/v1 이 호출되었어요. error 로그 #####################################");
+			// return "<h1>number 값이 -1로 요청했습니다.</h1>";
         }else if(number == 0){ // warn 로그
             log.warn("/aws/v1 이 호출되었어요. warn 로그 #####################################");
+			//return "<h1>number 값이 0로 요청했습니다.</h1>";
         }
-        
-        return "<h1>aws v1</h1>";
+		return "<h1>number 값이 " + number+"로 요청했습니다.</h1>";
     }
 }
